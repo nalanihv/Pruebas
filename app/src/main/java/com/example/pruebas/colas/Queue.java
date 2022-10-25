405b4752efa22068456c34609d6b68a91a41e857
Nalanihv <nalanihevi@gmail.com>
2022-10-25 09:55:19 -0500

    inicio de Queue.java /colas

---
+++ app/src/main/java/com/example/pruebas/colas/Queue.java
@@ -0,0 +1,12 @@
+package com.example.pruebas.colas;
+
+public class Queue { //es como el main?, para usar los datos de esa clase
+    private QueueNode first;
+    private QueueNode last;
+
+    public Queue() {//por que la primera vez estan vacios -Se inicializa
+        first=null;
+        last=null;
+      //first=last=null  ;
+    }
+}
--- app/src/main/java/com/example/pruebas/colas/colas.java
+++ app/src/main/java/com/example/pruebas/colas/colas.java
@@ -4,5 +4,6 @@ public class colas {
     /*carc. FIFO. primero que entra primero que sale
     * ej.mess;tareas a realizar como una lista;priodidades. sist.operativo
     * insertar.extremo final
-    * quita. por el frente */
+    * quita. por el frente
+    * */
 }
