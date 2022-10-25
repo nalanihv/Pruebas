e6a01b0aeb589fcd6949458fc80e69fda9a56b23
Nalanihv <nalanihevi@gmail.com>
2022-10-25 09:41:16 -0500

    definicion de cola en estructura de datos

---
+++ app/src/main/java/com/example/pruebas/colas/colas.java
@@ -0,0 +1,8 @@
+package com.example.pruebas.colas;
+
+public class colas {
+    /*carc. FIFO. primero que entra primero que sale
+    * ej.mess;tareas a realizar como una lista;priodidades. sist.operativo
+    * insertar.extremo final
+    * quita. por el frente */
+}
